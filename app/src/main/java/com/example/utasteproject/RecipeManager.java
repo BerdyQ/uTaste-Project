d1880ad4b7c5eb3409bd6ce5cbe1257588ce5f5a
Berdy Querette <berdy.querette@gmail.com>
2025-11-25 21:31:06 -0500

    Created the class layout for UserManager

--- app/src/main/java/com/example/utasteproject/RecipeManager.java
+++ app/src/main/java/com/example/utasteproject/RecipeManager.java
@@ -1,4 +1,8 @@
 package com.example.utasteproject;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class RecipeManager {
+
 }
--- app/src/main/java/com/example/utasteproject/UserManager.java
+++ app/src/main/java/com/example/utasteproject/UserManager.java
@@ -1,4 +1,22 @@
 package com.example.utasteproject;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class UserManager {
+    public LocalDateTime addUser(User user){
+        return LocalDateTime.now();
+    }
+    public User authenticateUser(String email, String password){
+        return null;
+    }
+    public User getUserByEmail(String email){
+
+    }
+    public void removerUser(User user){
+
+    }
+    public User[] listUsers(){
+
+    }
 }
