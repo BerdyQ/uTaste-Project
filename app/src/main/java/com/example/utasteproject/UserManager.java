303ba10d56b96295d04a5c3b7840f87a63c60608
Berdy Querette <berdy.querette@gmail.com>
2025-11-26 00:32:33 -0500

    Implemented UserManager

--- app/src/main/java/com/example/utasteproject/UserManager.java
+++ app/src/main/java/com/example/utasteproject/UserManager.java
@@ -2,21 +2,35 @@ package com.example.utasteproject;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class UserManager {
-    public LocalDateTime addUser(User user){
-        return LocalDateTime.now();
+    private HashMap<String,User> users = new HashMap<>();
+
+    public boolean addUser(User user){
+        if(user == null){return false;}
+        else if(users.containsKey(user.getEmail())){return false;}
+        users.put(user.getEmail(),user);
+            return true;
     }
     public User authenticateUser(String email, String password){
+        User user = users.get(email);
+        if(user != null && user.authenticate(email,password)){
+            return user;
+        }
         return null;
     }
     public User getUserByEmail(String email){
-
+        return users.get(email);
     }
-    public void removerUser(User user){
-
+    public boolean removeUser(String email){
+        if(!users.containsKey(email)){
+            return false;
+        }
+        users.remove(email);
+        return true;
     }
-    public User[] listUsers(){
-
+    public ArrayList<User> listUsers(){
+        return new ArrayList<>(users.values());
     }
 }
